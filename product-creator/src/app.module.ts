4abe08d8940a00ed7ee28f28c76da35dd3f144cd
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2021-10-29 15:55:05 +0200

    #1 app.module.ts and main.ts update

--- product-creator/src/app.module.ts
+++ product-creator/src/app.module.ts
@@ -4,16 +4,18 @@ import { ProductRequest } from './models/product-request';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductRequestService } from './services/product-request/product-request.service';
 
+const dbSocketAddr = process.env.DB_HOST?.split(':');
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProductRequest]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'database',
-      port: 5432,
-      username: 'SI5-CLOUD',
-      password: 'SI5-CLOUD',
-      database: 'SI5-CLOUD',
+      host: dbSocketAddr?dbSocketAddr[0]:'database',
+      port: dbSocketAddr?+dbSocketAddr[1]:5432,
+      username: process.env.DB_USER||'SI5-CLOUD',
+      password: process.env.DB_PASS||'SI5-CLOUD',
+      database: process.env.DB_NAME||'SI5-CLOUD',
       entities: [ProductRequest],
       synchronize: true,
   }),

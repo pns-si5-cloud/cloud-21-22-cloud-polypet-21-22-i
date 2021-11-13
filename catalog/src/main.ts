6bbec383b2c689b472005bb233614dda48b7f44b
TOGNETTI Yohann <56682531+Idrash@users.noreply.github.com>
2021-11-13 11:51:42 +0100

    allow cors origin

--- catalog/src/main.ts
+++ catalog/src/main.ts
@@ -5,6 +5,7 @@ const dbSocketAddr = process.env.DB_HOST?.split(':');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3003);
   console.log("-------------------------- CATALOG -------------------------");
 }

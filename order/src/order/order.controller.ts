7a38d22765740ef004f7e245fb940d4c6208f26f
nersissian <tigran.nersissian@etu.unice.fr>
2021-11-14 08:04:46 +0100

    #5 ajout controller pour le mock bank + vérification value null via pipe

--- order/src/order/order.controller.ts
+++ order/src/order/order.controller.ts
@@ -1,23 +1,23 @@
 import { Controller ,Post,Body} from '@nestjs/common';
 import { OrderService } from './order.service';
-
+import { ParseNotNullPipe } from 'src/pipe/parse-not-null.pipe';
 @Controller('order')
 export class OrderController {
     constructor(private readonly orderService: OrderService){
     }
     
     @Post('proceed-to-payment')
-    proceedToPayment(@Body('bankCardID') bankCardID:string, @Body('shoppingCartID') shoppingCartID:string, @Body('clientID') clientID:string,@Body('address') address:string,@Body('billingAddress') billingAddress:string) {
-        console.log("[order][proceedToPayment] shoppingCartID:string "+ shoppingCartID + "clientID:string "+clientID + " address:string "+address+" billingAddress:string "+ billingAddress);
+    proceedToPayment(@Body('account',ParseNotNullPipe) account:string,@Body('bankCardID',ParseNotNullPipe) bankCardID:string, @Body('shoppingCartID',ParseNotNullPipe) shoppingCartID:string, @Body('clientID',ParseNotNullPipe) clientID:string,@Body('address',ParseNotNullPipe) address:string,@Body('billingAddress',ParseNotNullPipe) billingAddress:string) {
+        console.log("[order][proceedToPayment] account:"+ account +" shoppingCartID:string "+ shoppingCartID + "clientID:string "+clientID + " address:string "+address+" billingAddress:string "+ billingAddress);
 
-        return this.orderService.proceedToPayment(bankCardID,shoppingCartID,clientID,address,billingAddress);
+        return this.orderService.proceedToPayment(account,bankCardID,shoppingCartID,clientID,address,billingAddress);
     }
     
     @Post('validation')
-    validation(@Body('shoppingCartID') shoppingCartID:string) {
-        console.log("[order][validation] shoppingCartID:string "+ shoppingCartID);
+    validation(@Body('status',ParseNotNullPipe) status:string,@Body('deliveryID',ParseNotNullPipe) deliveryID:string) {
+        console.log("[order][validation] status:string "+ status + " deliveryID " + deliveryID);
 
-        return this.orderService.validation(shoppingCartID);
+        return this.orderService.validation(status,deliveryID);
     }
 
 }

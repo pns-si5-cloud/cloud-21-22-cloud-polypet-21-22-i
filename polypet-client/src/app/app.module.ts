2b1e0895635e01583e03dbbe89d7f6510668af17
TOGNETTI Yohann <56682531+Idrash@users.noreply.github.com>
2021-11-21 22:56:51 +0100

    correction porlbeme mime

--- polypet-client/src/app/app.module.ts
+++ polypet-client/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './components/home/home.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { ProductListComponent } from './components/catalog/product-list/product-list.component';
@@ -85,7 +84,6 @@ import { PaymentPanelComponent } from './order/payment/payment-panel/payment-pan
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    FlexLayoutModule,
     HttpClientModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),

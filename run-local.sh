fe9c0158806342fb9e441c20a809b5882ed32675
TOGNETTI Yohann <56682531+Idrash@users.noreply.github.com>
2021-11-24 18:21:42 +0100

    delete old file

--- run-local.sh
+++
@@ -1,4 +0,0 @@
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd catalog | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd order | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd product-creator | npm run start:dev"&
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd shopping-cart | npm run start:dev"
